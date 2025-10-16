2e32a99b56a7fa011283dac71e2116b06a7560cc
aj-leal <adrian.leal23@gmail.com>
2025-10-16 23:49:41 +0800

    Updated keymap for inserting emoji

--- init.lua
+++ init.lua
@@ -466,7 +466,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>em', ':Emoji<CR>', { desc = 'Insert [em]oji' })
+      vim.keymap.set('i', 'em', '<Esc>:Emoji<CR>', { desc = 'Insert [em]oji' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
