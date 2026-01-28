d122144de9b956db12d41d1a7af39091306890b4
aj-leal <adrian.leal23@gmail.com>
2026-01-28 22:42:26 +0800

    Comment out custom keymap for fuzzy finding text in current buffer

--- init.lua
+++ init.lua
@@ -466,7 +466,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sa', ':Telescope current_buffer_fuzzy_find<CR>', { desc = '[S]e[A]rch current buffer' })
+      -- vim.keymap.set('n', '<leader>sa', ':Telescope current_buffer_fuzzy_find<CR>', { desc = '[S]e[A]rch current buffer' })
       vim.keymap.set('n', 'em', ':Emoji<CR>', { desc = 'Insert [em]oji' })
 
       -- Slightly advanced example of overriding default behavior and theme
